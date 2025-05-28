fc55b56443715d08b859da4f73d1d6170fc58b97
mlintelman <michaellintelman03@gmail.com>
2025-05-28 13:55:09 -0400

    Deleted examples, added serving size to FoodItem

--- FoodApi/Models/FoodItem.cs
+++ FoodApi/Models/FoodItem.cs
@@ -5,6 +5,7 @@
         public long id {  get; set; }
         public string? name { get; set; }
         public string? description { get; set; } = null;
+        public float servingSize { get; set; } // in grams
         public float calories {  get; set; }
         public float carbs { get; set; }
         public float protein { get; set; }
